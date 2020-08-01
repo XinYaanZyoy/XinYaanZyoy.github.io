47208b065136a6714451c81ec7209d9849ce9b50
心言自由 <XinYaanZyoy@gmail.com>
2020-08-01 23:54:07 +0530

    Update diary.sh

--- scripts/diary.sh
+++ scripts/diary.sh
@@ -19,7 +19,7 @@ if [ -d "$DIR" ]; then
             echo found $month
             if [ -f "$DIR/$year/$month/$date.txt" ]; then
                 fline = "$(sed -n '1{/^TIMESTAMP/p};q' "$DIR/$year/$month/$date.txt")";
-                echo fline;
+                echo $fline;
                 if [[ "$fline" == "$keyterm" ]]; then
                     echo found today entry at $DIR/$year/$month/$date.txt;
                     head -n 1 $DIR/$year/$month/$date.txt;
