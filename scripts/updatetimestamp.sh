438223c94dd6192660d1b954eca0369a2ba12df3
心言自由 <XinYaanZyoy@gmail.com>
2020-08-01 19:25:48 +0530

    Update updatetimestamp.sh

--- scripts/updatetimestamp.sh
+++ scripts/updatetimestamp.sh
@@ -3,10 +3,10 @@ echo Start of stamp updatation;
 stamp=$(date);
 echo $stamp;
 echo $'\n'"git status: $(git status)"$'\n';
-sed -i -e "s/\(<i>\).*\(<\/i>\)/<i>$stamp<\/i>/g" "_layouts/default.html"
+sed -i -e "s/\(<i>\).*\(<\/i>\)/<i>$stamp<\/i>/g" "_includes/header.html"
 echo $'\n'"git status: $(git status)"$'\n';
 git config --global user.name XinYaanZyoy && git config --global user.email XinYaanZyoy@gmail.com
-git add "_layouts/default.html"
+git add "_includes/header.html"
 git commit -m "timestamp: $stamp"
 git push "https://XinYaanZyoy:$GH_PAT@github.com/XinYaanZyoy/XinYaanZyoy.github.io.git" HEAD:master
 echo End Of stamp updatation;
