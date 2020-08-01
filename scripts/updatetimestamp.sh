9c3e5a66731e632f3332eee66c524f911a5558e3
心言自由 <XinYaanZyoy@gmail.com>
2020-08-01 08:41:50 +0530

    Update updatetimestamp.sh

--- scripts/updatetimestamp.sh
+++ scripts/updatetimestamp.sh
@@ -7,8 +7,7 @@ sed -i -e "s/\(<i>\).*\(<\/i>\)/<i>$stamp<\/i>/g" index.md
 echo $'\n'"git status: $(git status)"$'\n';
 if [ "$status" = true ]; then
             git config --global user.name XinYaanZyoy && git config --global user.email XinYaanZyoy@gmail.com
-            git add index.md
-            git commit -m "timestamp: $stamp"
+            git commit -a -m "timestamp: $stamp"
             git push "https://XinYaanZyoy:$GH_PAT@github.com/XinYaanZyoy/XinYaanZyoy.github.io.git" HEAD:master
 fi
 echo End Of stamp updatation;
