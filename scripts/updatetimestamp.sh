e09cccc8889c776d11842df212aad8229330b6be
心言自由 <XinYaanZyoy@gmail.com>
2020-08-02 00:44:46 +0530

    Update updatetimestamp.sh

--- scripts/updatetimestamp.sh
+++ scripts/updatetimestamp.sh
@@ -8,5 +8,6 @@ echo $'\n'"git status: $(git status)"$'\n';
 git config --global user.name XinYaanZyoy && git config --global user.email XinYaanZyoy@gmail.com
 git add "_includes/header.html"
 git commit -m "timestamp: $stamp"
+git pull --rebase origin master
 git push "https://XinYaanZyoy:$GH_PAT@github.com/XinYaanZyoy/XinYaanZyoy.github.io.git" HEAD:master
 echo End Of stamp updatation;
