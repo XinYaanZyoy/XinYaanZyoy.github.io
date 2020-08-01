9d16022d7e96424736d0bc9c584afb3b90d69492
心言自由 <XinYaanZyoy@gmail.com>
2020-08-01 22:17:12 +0530

    Create diary.sh

---
+++ scripts/diary.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+echo Start of diary entry;
+stamp=$(date);
+year=$(date +%Y);
+month=$(date +%b);
+date=$(date +%d);
+DIR="diary";
+status=false;
+
+echo $stamp;
+echo $'\n'"git status: $(git status)"$'\n';
+
+if [ -d "$DIR" ]; then
+    echo found diary
+    if [ -d "$year" ]; then
+        echo found $year
+        if [ -d "$month" ]; then
+            echo found $month
+            if [ -f "$date" ]; then
+                echo found today's entry at $DIR/$year/$month/$date.txt
+                echo timestamping...
+                sed -i "1s/.*/$stamp/" "$DIR/$year/$month/$date.txt";
+                status=true;
+            else
+                echo not found today's entry!
+            fi
+        else
+            echo not found $month
+        fi
+    else
+        echo not found $year
+    fi
+else
+    echo $DIR not found
+fi
+
+if [ "$status" = true ]; then
+    echo $'\n'"git status: $(git status)"$'\n';
+    git config --global user.name XinYaanZyoy && git config --global user.email XinYaanZyoy@gmail.com
+    git add "diary/*"
+    git commit -m "diary-entry: $stamp"
+    git push "https://XinYaanZyoy:$GH_PAT@github.com/XinYaanZyoy/XinYaanZyoy.github.io.git" HEAD:master
+    echo End Of diary entry;
+else
+    echo something went wrong while timestamping!
+fi
