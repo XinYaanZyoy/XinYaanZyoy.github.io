238c608933d6bf3cc10f0ffa1a91fcc85e3ca1fc
心言自由 <XinYaanZyoy@gmail.com>
2020-08-01 23:13:39 +0530

    Update diary.sh

--- scripts/diary.sh
+++ scripts/diary.sh
@@ -18,15 +18,9 @@ if [ -d "$DIR" ]; then
             echo found $month
             if [ -f "$DIR/$year/$month/$date.txt" ]; then
                 echo found today entry at $DIR/$year/$month/$date.txt;
-                fline="$(head -n 1 filename)";
-                echo fline is $fline
-                if [ "$fline" = "TIMESTAMP" ]; then
-                    echo timestamping...;
-                    sed -i "1s/.*/$stamp/" "$DIR/$year/$month/$date.txt";
-                    status=true;
-                else
-                    echo already time stamped
-                    status=false;
+                echo timestamping...;
+                sed -i "1s/.*/$stamp/" "$DIR/$year/$month/$date.txt";
+                status=true;
                 fi
             else
                 echo not found todays entry!
@@ -49,5 +43,5 @@ if [ "$status" = true ]; then
     git push "https://XinYaanZyoy:$GH_PAT@github.com/XinYaanZyoy/XinYaanZyoy.github.io.git" HEAD:master
     echo End Of diary entry;
 else
-    echo either there is no need of timestamping or something went wrong when timestamping!
+    echo something went wrong when timestamping!
 fi
